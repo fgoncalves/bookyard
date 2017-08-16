4041a350726f7ba2aaba26480bcb204fbe306bf9
fgoncalves <fgoncalves@babbel.com>
2017-08-16 09:24:13 +0200

    Wires up lifecycle wth view model

--- app/src/main/kotlin/com/github/fgoncalves/bookyard/presentation/screens/SplashScreen.kt
+++ app/src/main/kotlin/com/github/fgoncalves/bookyard/presentation/screens/SplashScreen.kt
@@ -1,20 +1,33 @@
 package com.github.fgoncalves.bookyard.presentation.screens
 
+import android.arch.lifecycle.LifecycleRegistry
+import android.arch.lifecycle.LifecycleRegistryOwner
 import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
 import com.github.fgoncalves.bookyard.R
 import com.github.fgoncalves.bookyard.databinding.SplashScreenBinding
 import com.github.fgoncalves.bookyard.presentation.viewmodels.SplashScreenViewModel
 
-class SplashScreen : BaseScreen<SplashScreenBinding>() {
+class SplashScreen : BaseScreen<SplashScreenBinding>(), LifecycleRegistryOwner {
   override val layout: Int = R.layout.splash_screen
+  private val lifecycleRegistry = LifecycleRegistry(this)
+  private var viewModel: SplashScreenViewModel? = null
 
   companion object {
     @JvmStatic
     fun newInstance(): SplashScreen = SplashScreen()
   }
 
-  override fun applyBindings(binding: SplashScreenBinding) {
-    val viewModel = ViewModelProviders.of(this, viewModelFactory)[SplashScreenViewModel::class.java]
-    binding.viewModel = viewModel
+  override fun applyBindings(viewDataBinding: SplashScreenBinding) {
+    viewModel = ViewModelProviders.of(this, viewModelFactory)[SplashScreenViewModel::class.java]
+    viewDataBinding.viewModel = viewModel
+    lifecycle.addObserver(viewModel)
   }
+
+  override fun onActivityCreated(savedInstanceState: Bundle?) {
+    super.onActivityCreated(savedInstanceState)
+    viewModel?.onActivityCreated()
+  }
+
+  override fun getLifecycle(): LifecycleRegistry = lifecycleRegistry
 }
--- app/src/main/kotlin/com/github/fgoncalves/bookyard/presentation/viewmodels/SplashScreenViewModel.kt
+++ app/src/main/kotlin/com/github/fgoncalves/bookyard/presentation/viewmodels/SplashScreenViewModel.kt
@@ -1,5 +1,10 @@
 package com.github.fgoncalves.bookyard.presentation.viewmodels
 
+import android.arch.lifecycle.Lifecycle.Event.ON_DESTROY
+import android.arch.lifecycle.Lifecycle.Event.ON_START
+import android.arch.lifecycle.Lifecycle.Event.ON_STOP
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
 import android.arch.lifecycle.ViewModel
 import android.util.Log
 import android.view.View.OnClickListener
@@ -8,7 +13,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import javax.inject.Inject
 
-abstract class SplashScreenViewModel : ViewModel() {
+abstract class SplashScreenViewModel : ViewModel(), LifecycleObserver {
   abstract fun onScreenStart()
 
   abstract fun onScreenStop()
@@ -27,24 +32,27 @@ class SplashScreenViewModelImpl @Inject constructor(
     val googleOps: GoogleSignInOptions,
     val firebaseAuth: FirebaseAuth) : SplashScreenViewModel() {
 
+  @OnLifecycleEvent(ON_START)
   override fun onScreenStart() {
     TODO(
         "Make this lifecycle")
   }
 
+  @OnLifecycleEvent(ON_STOP)
   override fun onScreenStop() {
+    TODO(
+        "Make this lifecycle")
+  }
+
+  @OnLifecycleEvent(ON_DESTROY)
+  override fun onDestroy() {
     TODO(
         "Make this lifecycle")
   }
 
   override fun onActivityCreated() {
     TODO(
-        "Make this lifecycle")
-  }
-
-  override fun onDestroy() {
-    TODO(
-        "Make this lifecycle")
+        "This doesn't work with lifecycle yet")
   }
 
   override fun onSignedIn() {
