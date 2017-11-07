a35a186eb478551263dea212b699b85ec046b268
Fred <frederico.lopes.goncalves@gmail.com>
2017-11-07 23:47:47 +0100

    refactors user service

--- app/src/main/kotlin/com/github/fgoncalves/bookyard/data/UserService.kt
+++ app/src/main/kotlin/com/github/fgoncalves/bookyard/data/UserService.kt
@@ -12,47 +12,41 @@ import io.reactivex.Single
 import javax.inject.Inject
 
 interface UserService {
-  fun get(uuid: String): Maybe<User>
+    fun get(uuid: String): Maybe<User>
 
-  fun createOrUpdate(user: User): Single<User>
+    fun createOrUpdate(user: User): Single<User>
 }
 
 class UserServiceImpl @Inject constructor(
-    @UsersDatabase val userDatabaseReference: DatabaseReference
+        @UsersDatabase private val userDatabaseReference: DatabaseReference
 ) : UserService {
-  override fun get(uuid: String): Maybe<User>
-      = Maybe.create {
-    if (it.isDisposed) return@create
+    override fun get(uuid: String): Maybe<User> = Maybe.create {
+        it.takeUnless { it.isDisposed }?.let {
+            userDatabaseReference.child(uuid).addListenerForSingleValueEvent(
+                    object : ValueEventListener {
+                        override fun onCancelled(error: DatabaseError?) {
+                            if (error == null) {
+                                it.onError(RuntimeException("Couldn't get user from firebase"))
+                                return
+                            }
+                            it.onError(FirebaseDatabaseException(error))
+                        }
 
-    userDatabaseReference.child(uuid).addListenerForSingleValueEvent(object : ValueEventListener {
-      override fun onCancelled(error: DatabaseError?) {
-        if (error == null) {
-          it.onError(RuntimeException("Couldn't get user from firebase"))
-          return
+                        override fun onDataChange(snapshot: DataSnapshot?) {
+                            if (snapshot == null) {
+                                it.onError(RuntimeException("Snapshot is null"))
+                                return
+                            }
+
+                            snapshot.getValue(User::class.java)?.run { it.onSuccess(this) }
+                            it.onComplete()
+                        }
+                    })
         }
-        it.onError(FirebaseDatabaseException(error))
-      }
+    }
 
-      override fun onDataChange(snapshot: DataSnapshot?) {
-        if (snapshot == null) {
-          it.onError(RuntimeException("Snapshot is null"))
-          return
-        }
-
-        val user = snapshot.getValue(User::class.java)
-
-        if (user != null) it.onSuccess(user)
-        it.onComplete()
-      }
-    })
-  }
-
-  override fun createOrUpdate(user: User): Single<User>
-      = Single.fromCallable {
-    val userDatabaseReference = userDatabaseReference.child(user.uuid)
-    userDatabaseReference.setValue(user)
-    return@fromCallable user
-  }
-      .flatMapMaybe { get(it.uuid) }
-      .toSingle()
+    override fun createOrUpdate(user: User): Single<User> =
+            Single.fromCallable { user.apply { userDatabaseReference.child(uuid).setValue(this) } }
+                    .flatMapMaybe { get(it.uuid) }
+                    .toSingle()
 }
